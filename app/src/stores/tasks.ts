566c3f278ca7bf82007b87db7725a9b1ad2a5b2e
Willem Franco <willemfranco@gmail.com>
2023-05-16 12:29:46 -0500

    Finishing main functionality in task view

--- app/src/stores/tasks.ts
+++ app/src/stores/tasks.ts
@@ -1,26 +1,54 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export interface Task {
-  id?: number
+  id: number
   title: string
+  status: boolean,
+  created_at: string;
+  updated_at: string;
+}
+
+interface CreateTask {
+  title: string
+  status: boolean
+}
+
+interface UpdateTask {
+  id: number
   status: boolean
 }
 
 export const useTasksStore = defineStore('tasks', () => {
   const tasks = ref<Task[]>([])
 
-  function addTask(task: Task) {
-    tasks.value.push(task);
+  async function getTasks() {
+    const { data } = await axios.get<Task[]>('http://localhost/api/tasks')
+    tasks.value = data
   }
 
-  function removeTask(id: number) {
-    tasks.value = tasks.value.filter((task) => task.id !== id)
+  async function addTask(task: CreateTask) {
+    await axios.post<Task>('http://localhost/api/tasks', task)
+    getTasks();
+  }
+
+  async function updateTask({id, status}: UpdateTask) {
+    console.log('update', {id, status})
+    await axios.put<Task>(`http://localhost/api/tasks/${id}`, {status})
+    getTasks();
+  }
+
+  async function removeTask(id: number) {
+    await axios.delete(`http://localhost/api/tasks/${id}`)
+    getTasks();
   }
 
   return {
     tasks,
     addTask,
+    getTasks,
+    updateTask,
     removeTask
   }
 })
--- app/src/views/TasksView.vue
+++ app/src/views/TasksView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container p-3">
-    <h1>Testing bootstrap --- input with button</h1>
-    <form @submit.prevent="add">
-      <div class="input-group">
+    <h1>
+      Tasks
+      <small class="text-secondary">({{store.tasks.length}})</small>
+    </h1>
+    <form @submit.prevent="addTask">
+      <div class="input-group my-1">
         <input
           v-model="title"
           type="text"
@@ -12,23 +15,28 @@
           aria-describedby="search-addon"
         />
         <button
-          class="btn btn-outline-secondary"
+          class="btn btn-outline-primary"
           type="button"
           id="search-addon"
+          :disabled="saving"
         >
           + Add
         </button>
       </div>
     </form>
 
-    <ul v-if="tasks.length" class="list-group">
+    {{ tasks }}
+    <ul v-if="store.tasks.length" class="list-group mt-5">
       <li
-        v-for="(task, index) in tasks"
-        :key="index"
+        v-for="task in store.tasks"
+        :key="task.id"
         class="list-group-item d-flex justify-content-between align-items-center"
+        :class="{'list-group-item-success': task.status}"
       >
-        {{ task.title }}
-        <button @click="removeTask(index)" class="btn btn-sm btn-danger">
+        <span class="title w-100" :class="{'text-decoration-line-through': task.status}" @click="toggleStatus(task)">
+          {{ task.title }}
+        </span>
+        <button @click="removeTask(task.id)" class="btn btn-sm btn-danger">
           X
         </button>
       </li>
@@ -37,27 +45,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import { useTasksStore } from "../stores/tasks";
+import { defineComponent, onMounted, ref } from "vue";
+import { Task, useTasksStore } from "../stores/tasks";
 
 export default defineComponent({
   name: "TasksView",
   setup() {
-    const id = ref(1);
+    const saving = ref(false);
     const title = ref('');
-    const { tasks, addTask, removeTask } = useTasksStore();
-    function add() {
-      addTask({
-        id: id.value,
-        title: title.value,
-      })
-      id.value++;
+    const store = useTasksStore();
+
+    onMounted(() => {
+      store.getTasks();
+    });
+
+    async function addTask() {
+      saving.value = true;
+      try {
+        await store.addTask({
+          title: title.value,
+          status: false,
+        })
+        title.value = ''
+      } finally {
+        saving.value = false;
+      }
     }
-    return { tasks, title, add, removeTask };
+    async function toggleStatus({id, status}: Task) {
+      // change task status
+      store.updateTask({id, status: !status})
+    }
+    async function removeTask(id: number) {
+      saving.value = true;
+      try {
+        await store.removeTask(id)
+      } finally {
+        saving.value = false;
+      }
+    }
+    return { saving, store, title, addTask, toggleStatus, removeTask };
   },
 });
 </script>
 
 
-<style>
+<style scoped>
+.title {
+  cursor: pointer;
+}
 </style>
