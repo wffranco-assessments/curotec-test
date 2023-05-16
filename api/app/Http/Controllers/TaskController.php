249d5e8a81412f8ad679af9c23f971bafc1c0990
Willem Franco <willemfranco@gmail.com>
2023-05-16 10:08:39 -0500

    First iteration

---
+++ api/app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskCreate;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskCreate $request)
+    {
+        return Task::create($request);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
